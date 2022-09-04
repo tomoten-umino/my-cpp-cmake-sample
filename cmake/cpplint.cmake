dc0d5e601f67bcdc67176981ef005ccb24b847ba
tomoten-umino <tomoten.umino@gmail.com>
2022-09-04 21:57:31 +0900

    update run metrixpp

--- cmake/cpplint.cmake
+++ cmake/cpplint.cmake
@@ -1,3 +1,12 @@
+# search c++ files
+file(GLOB_RECURSE ALL_SOURCE_FILES *.cpp *.hpp *.c *.h)
+foreach (SOURCE_FILE ${ALL_SOURCE_FILES})
+    string(FIND ${SOURCE_FILE} ${PROJECT_BINARY_DIR} PROJECT_TRDPARTY_DIR_FOUND)
+    if (NOT ${PROJECT_TRDPARTY_DIR_FOUND} EQUAL -1)
+        list(REMOVE_ITEM ALL_SOURCE_FILES ${SOURCE_FILE})
+    endif ()
+endforeach ()
+
 # find command cpplint
 find_program(CPP_LINT cpplint)
 
@@ -6,5 +15,5 @@ add_custom_target(
     cpplint
     COMMAND cd ${CMAKE_CURRENT_SOURCE_DIR} &&
             ${CPP_LINT} --filter=-legal/copyright
-            `git diff --name-only`
+            ${ALL_SOURCE_FILES}
 )
--- cmake/metrixpp.cmake
+++ cmake/metrixpp.cmake
@@ -6,15 +6,16 @@ add_custom_target(
     metrixpp
     # analysis
     COMMAND cd ${CMAKE_CURRENT_SOURCE_DIR} &&
-            ${METRIXPP} collect --std.code.lines.code 
+            ${METRIXPP} collect --log-level=WARNING
+                                --std.code.lines.code 
                                 --std.code.complexity.maxindent
                                 --std.code.complexity.cyclomatic
+                                --exclude-files=CMakeCXXCompilerId.cpp
                                 --db-file=${PROJECT_BINARY_DIR}/metrixpp.db
-                                `git diff --name-only`
     # export from db to csv
     COMMAND ${METRIXPP} export --db-file=./metrixpp.db > metrics-result.csv
     # check max indent
-    COMMAND ${METRIXPP} limit --log-level=ERROR --db-file=metrixpp.db --max-limit=std.code.complexity:maxindent:4 | tee max_indent_error.txt
+    COMMAND ${METRIXPP} limit --log-level=WARNING --db-file=metrixpp.db --max-limit=std.code.complexity:maxindent:4 | tee max_indent_error.txt
     # check max cyclomatic 
-    COMMAND ${METRIXPP} limit --log-level=ERROR --db-file=metrixpp.db --max-limit=std.code.complexity:cyclomatic:10 | tee max_cyclomatic_error.txt
+    COMMAND ${METRIXPP} limit --log-level=WARNING --db-file=metrixpp.db --max-limit=std.code.complexity:cyclomatic:10 | tee max_cyclomatic_error.txt
 )
