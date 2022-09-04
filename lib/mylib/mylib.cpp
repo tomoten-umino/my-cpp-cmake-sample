0d18c62ed1561c00d82001e9146c052581999029
tomoten-umino <tomoten.umino@gmail.com>
2022-09-04 23:38:24 +0900

    add depp_nested_hello

--- lib/mylib/mylib.cpp
+++ lib/mylib/mylib.cpp
@@ -18,4 +18,20 @@ void Mylib::nested_hello() {
     }
 }
 
+void Mylib::deep_nested_hello() {
+    // dummy val
+    std::string tmp1("tmp1");
+    std::string tmp2("tmp2");
+    std::string tmp3("tmp3");
+    std::string tmp4("tmp4");
 
+    if ( tmp1 == "tmp1" ) {
+        if ( tmp2 == "tmp2" ) {
+            if ( tmp3 == "tmp3" ) {
+                if ( tmp4 == "tmp4" ) {
+                    std::cout << "Hello world!" << std::endl;
+                }
+            }
+        }
+    }
+}
